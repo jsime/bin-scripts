0124fc6978514282e59b522310543c89b0ff7ea3
Jon Sime <jonsime@gmail.com>
2014-06-25 11:24:23 -0400

    fixup on xmonad autostart

--- xmonad-autostart.sh
+++ xmonad-autostart.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 HOST=$(hostname)
-
-xrdb -merge ~/.Xresources
+STARTUPD="${HOME}/.xmonad/startup.d"
+HOSTDOTD="${STARTUPD}/${HOST}.d"
 
 # startup scripts to be run on every host
-for s in `ls ~/.xmonad/startup.d/*`; do
-    if [ -x "$s" ]; then
-        ${s}
-    fi
-done
+if [[ -d "${STARTUPD}" ]]; then
+    for fn in `ls ${STARTUPD}`; do
+        if [[ -L "${STARTUPD}/${fn}" ]]; then
+            ${STARTUPD}/${fn}
+        fi
+    done
+fi
 
 # host-specific scripts, which should be symlinks into startup.d/available/
-if [ -d "~/.xmonad/startup.d/${HOST}.d" ]; then
-    for s in `ls ~/.xmonad/startup.d/${HOST}.d/*`; do
-        if [ -x "$s" ]; then
-            ${s}
+if [[ -d "${HOSTDOTD}" ]]; then
+    for fn in `ls ${HOSTDOTD}`; do
+        if [[ -L "${HOSTDOTD}/${fn}" ]]; then
+            ${HOSTDOTD}/${fn}
         fi
     done
 fi
