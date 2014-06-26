a9eb88c32c2b04f1f5fca5a7b1007f3e972d8724
Jon Sime <jonsime@gmail.com>
2014-06-25 20:51:20 -0400

    bash syntax fix

--- xmonad-autostart.sh
+++ xmonad-autostart.sh
@@ -5,18 +5,18 @@ STARTUPD="${HOME}/.xmonad/startup.d"
 HOSTDOTD="${STARTUPD}/${HOST}.d"
 
 # startup scripts to be run on every host
-if [[ -d "${STARTUPD}" ]]; then
+if [ -d "${STARTUPD}" ]; then
     for fn in `ls ${STARTUPD}`; do
-        if [[ -L "${STARTUPD}/${fn}" ]]; then
+        if [ -L "${STARTUPD}/${fn}" ]; then
             ${STARTUPD}/${fn}
         fi
     done
 fi
 
 # host-specific scripts, which should be symlinks into startup.d/available/
-if [[ -d "${HOSTDOTD}" ]]; then
+if [ -d "${HOSTDOTD}" ]; then
     for fn in `ls ${HOSTDOTD}`; do
-        if [[ -L "${HOSTDOTD}/${fn}" ]]; then
+        if [ -L "${HOSTDOTD}/${fn}" ]; then
             ${HOSTDOTD}/${fn}
         fi
     done
